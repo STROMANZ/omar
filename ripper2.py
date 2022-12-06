c96c4c9abd0dd57cb3e3a10630a25aaf629b9c19
STROMANZ <stromanz@gmail.com>
2022-12-06 15:26:34 +0100

    commit from up-2-date omar2 repo

--- ripper2.py
+++ ripper2.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python
 import tkinter as tk
-import os
 from tkinter.ttk import *
 from tkinter import filedialog
+import os
+import time
+import signal
+import shutil
+import subprocess
+from subprocess import Popen, PIPE
+import stat
+from threading import Thread
+
 
 geo_width = 1280
 geo_height = 57
 drives = {}
+output = "/media"
+
 
 def drive_config_reader():
     drive_config = open('.drive.config', 'r')
@@ -17,10 +27,12 @@ def drive_config_reader():
     drive_config.close()
     return value
 
+
 def drive_config_writer(number):
     drive_config = open('.drive.config', 'w')
     drive_config.write('%d' % number)
     drive_config.close()
+
 
 def output_config_reader():
     output_config = open('.output.config', 'r')
@@ -31,61 +43,159 @@ def output_config_reader():
     output_config.close()
     return path
 
+
 def output_config_writer(path):
     output_config = open('.output.config', 'w')
     output_config.write('%s' % path)
     output_config.close()
 
+
 def check_mount_point(path):
     if not os.path.ismount(path):
-        tk.messagebox.showinfo(title="Externe USB Check", message=f'{path} is geen USB-disk')
+        tk.messagebox.showinfo(title="Externe USB Check", message=f'\"{path}\" is geen USB-disk')
+
+
+def drive_exists(path):
+    try:
+        return stat.S_ISBLK(os.stat(path).st_mode)
+    except:
+        return False
 
 def create_directory(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-def queryOutputPath():
+
+def progress_indicator(deviceid, number):
+    drives[deviceid]['progress'] = number
+
+
+def checksum_from_file(file_name):
+    file = open(file_name, 'r')
+    line = file.readline()
+    sum = line.split()
+    return sum[0]
+
+
+def md5sum_compare(deviceid, media_md5, iso_md5):
+    if str(media_md5) != str(iso_md5):
+        drives[deviceid]['console_output'].insert("end-1c", "Het gemaakte iso bestand komt niet overeen met het \
+        orignele optische media")
+        window.update_idletasks()
+        return 1
+
+
+def queryoutputpath():
     global outpath
-    outpath = tk.filedialog.askdirectory(parent=window, initialdir="/media/", title='Selecteer output directory (USB-Disk)')
+    outpath = tk.filedialog.askdirectory(parent=window, initialdir="/media/",
+                                         title='Selecteer output directory (USB-Disk)')
     output_config_writer(outpath)
     check_mount_point(outpath)
 
-def applySettings(event):
+
+def applysettings(event):
     global drives
     global geo_height
-    new_geo_height = geo_height * slidervalue.get()
-    window.geometry(f'{geo_width}x{new_geo_height}')
 
-    drive_config_writer(slidervalue.get())
+    modifydrives = True
 
-    if slidervalue.get() < len(drives):
-        drives_rem(slidervalue.get())
-    elif slidervalue.get() > len(drives):
-        drives_add(slidervalue.get())
-    else:
+    if len(drives) == 0:
         pass
+    else:
+        for drive in range(len(drives)):
+            if drives[drive]['running'] == True:
+                tk.messagebox.showerror('Error', f'Aanpassing afgewezen, drive /dev/sr{drives[drive]["deviceID"]} \
+                is actief')
+                modifydrives = False
+
+    if modifydrives:
+        new_geo_height = geo_height * slidervalue.get()
+        window.geometry(f'{geo_width}x{new_geo_height}')
+
+        drive_config_writer(slidervalue.get())
+
+        if slidervalue.get() < len(drives):
+            drives_rem(slidervalue.get())
+        elif slidervalue.get() > len(drives):
+            drives_add(slidervalue.get())
+        else:
+            pass
+
 
 def drives_rem(numberofdrives):
     global drives
     for drive in range(numberofdrives):
         drives[drive].popitem()
 
-def handle_button_action_press(instanceID):
+
+def handle_button_action_press(deviceid):
+    runjob = True
+
     try:
-        id = int(drives[instanceID]['inputtxt'].get())
+        int(drives[deviceid]['inputtxt'].get())
     except:
         tk.messagebox.showerror('Error', 'Error: Dig-ID geen nummer')
     else:
-        outputdir = output_config_reader() + "/" + str(drives[instanceID]['inputtxt'].get()) + "/"
+        if drives[deviceid]['running'] == True:
+            tk.messagebox.showerror('Error', f'Niet gestart, drive /dev/sr{drives[deviceid]["deviceID"]} \
+            is al actief')
+            runjob = False
+
+    if not drive_exists("/dev/sr" + str(drives[deviceid]['deviceID'])):
+        tk.messagebox.showerror('Error', f'Niet gestart, drive /dev/sr{drives[deviceid]["deviceID"]} bestaat niet')
+        runjob = False
+
+    if runjob:
+        drives[deviceid]['running'] = True
+        outputdir = output_config_reader() + "/" + str(drives[deviceid]['inputtxt'].get()) + "/"
         create_directory(outputdir)
-        iso_name = outputdir + str(drives[instanceID]['inputtxt'].get()) + ".iso"
+        iso_name = outputdir + str(drives[deviceid]['inputtxt'].get()) + ".iso"
         print(iso_name)
         iso_md5_name = iso_name + ".md5"
         print(iso_md5_name)
         iso_md5_optical_name = iso_name + ".optical.md5"
         print(iso_md5_optical_name)
-        optical_device = "/dev/sr" + str(instanceID)
+        optical_device = "/dev/sr" + str(deviceid)
         print(optical_device)
+        progress_indicator(deviceid, 5)
+        dd = Popen(["dd", "if=" + optical_device, "of=" + iso_name, "conv=noerror"], stderr=PIPE)
+        while dd.poll() is None:
+            time.sleep(.3)
+            dd.send_signal(signal.SIGUSR1)
+            while 1:
+                line = dd.stderr.readline()
+                if 'bytes' in str(line):
+                    drives[deviceid]['console_output'].delete('1.0', tk.END)
+                    drives[deviceid]['console_output'].insert("end-1c", line)
+                    window.update_idletasks()
+                    break
+        progress_indicator(deviceid, 35)
+        with open(iso_md5_optical_name, 'w') as chksum_file:
+            subprocess.run(["md5sum", optical_device], stdout=chksum_file)
+        chksum_file.close()
+        md5_a = checksum_from_file(iso_md5_optical_name)
+        progress_indicator(deviceid, 65)
+        with open(iso_md5_name, 'w') as chksum_file:
+            subprocess.run(["md5sum", iso_name], stdout=chksum_file)
+        chksum_file.close()
+        md5_b = checksum_from_file(iso_md5_name)
+        md5sum_compare(deviceid, md5_a, md5_b)
+        progress_indicator(deviceid, 70)
+        drives[deviceid]['console_output'].insert("end-1c", "Checksum validatie succesvol" + '\n')
+        window.update_idletasks()
+        progress_indicator(deviceid, 75)
+        create_directory(outpath + "temp/")
+        subprocess.run(["fuseiso", iso_name, outpath + "temp/"])
+        shutil.copytree(outpath + "temp", outpath + "content")
+        subprocess.run(["fusermount", "-u", outpath + "temp"])
+        subprocess.run(["rm", "-rf", outpath + "temp/"])
+        progress_indicator(deviceid, 95)
+        subprocess.run(["eject"])
+        drives[deviceid]['console_output'].insert("end-1c", "Done")
+        progress_indicator(deviceid, 100)
+
+        drives[deviceid]['running'] = False
+
 
 def drives_add(numberofdrives):
     global drives
@@ -100,7 +210,11 @@ def drives_add(numberofdrives):
         drives[drive]['inputtxt'] = tk.Entry(window, width=30)
         drives[drive]['inputtxt'].grid(row=drive, column=2)
 
+        # thread_1 = Thread(target=handle_button_action_press(0))
+
         # Button Creation (needs work)
+        # Calling a function with an argument gets executed when adding to grid.
+        # Using a lambda function with variables, while iterating, causes all variable to be set to last iteration val.
         if drive == 0:
             button_action_0 = tk.Button(text="Run", relief='raised', command=lambda: handle_button_action_press(0))
             button_action_0.grid(row=drive, column=10, sticky='E')
@@ -146,19 +260,21 @@ def drives_add(numberofdrives):
         drives[drive]['progress'].grid(row=drive, column=7, columnspan=3, sticky='EW')
 
         # Console Output
-        drives[drive]['console_output'] = tk.Text(window, state='disabled', bg='black', fg='white', height=3, width=64, insertborderwidth=2)
+        drives[drive]['console_output'] = tk.Text(window, state='disabled', bg='black', fg='white',
+                                                  height=3, width=64, insertborderwidth=2)
         drives[drive]['console_output'].grid(row=drive, columnspan=3, column=3, sticky='E')
 
-def openSettingsWindow():
-    SettingsWindow = tk.Toplevel(window)
-    SettingsWindow.lift()
-    SettingsWindow.title("Settings")
-    SettingsWindow.geometry("200x100")
+
+def open_settings_window():
+    settings_window = tk.Toplevel(window)
+    settings_window.lift()
+    settings_window.title("Settings")
+    settings_window.geometry("200x100")
 
     global slidervalue
 
     slider = tk.Scale(
-        SettingsWindow,
+        settings_window,
         variable=slidervalue,
         from_=1,
         to=12,
@@ -166,22 +282,23 @@ def openSettingsWindow():
     )
     slider.pack()
 
-    outputButton = tk.Button(
-        SettingsWindow,
+    output_button = tk.Button(
+        settings_window,
         text="Opslag Locatie",
-        command=queryOutputPath
+        command=queryoutputpath
     )
-    outputButton.pack()
+    output_button.pack()
 
-    applyButton = tk.Button(
-        SettingsWindow,
+    apply_button = tk.Button(
+        settings_window,
         text="Toepassen",
-        command=SettingsWindow.destroy
+        command=settings_window.destroy
     )
-    applyButton.bind('<Button-1>', applySettings)
-    applyButton.pack()
+    apply_button.bind('<Button-1>', applysettings)
+    apply_button.pack()
 
-#-------------------------------------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------------------------------------
 
 window = tk.Tk()
 window.title("Optical Media Archive Ripper")
@@ -193,7 +310,7 @@ file_menu = tk.Menu(menubar)
 
 file_menu.add_command(
     label='Settings',
-    command=openSettingsWindow,
+    command=open_settings_window,
 )
 
 file_menu.add_command(
@@ -211,7 +328,7 @@ check_mount_point(output_config_reader())
 
 slidervalue = tk.IntVar()
 slidervalue.set(int(drive_config_reader()))
-applySettings(True)
+applysettings(True)
 
 if __name__ == '__main__':
     window.mainloop()
